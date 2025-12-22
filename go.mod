d9ea08035af5566dfba7bf79c45de3809e4e1d77
aryan <therealaryannagpal@gmail.com>
2025-12-22 11:27:10 -0500

    Added basic TCP listener logic

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/realaryann/keystore
+
+go 1.25.1
--- main.go
+++ main.go
@@ -2,8 +2,39 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net"
+	"os"
 )
 
 func main() {
-	fmt.Println("KeyStore")
+	fmt.Println("KeyStore Server")
+	tcpl, err := net.Listen("tcp", ":6000")
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	conn, err := tcpl.Accept()
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	defer conn.Close()
+
+	// Infinite Loop to answer connections
+	for {
+		buffer := make([]byte, 1024)
+
+		_, err = conn.Read(buffer)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("Error: ", err.Error())
+			os.Exit(1)
+		}
+
+		conn.Write([]byte("+OK\r\n"))
+	}
+
 }
